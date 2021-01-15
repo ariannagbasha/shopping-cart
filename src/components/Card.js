778c44a3dce561d50caf6585d5ba1389c79b0d35
TJ Hindman <tjhindman@TJs-MacBook-Pro.attlocal.net>
2021-01-15 15:19:12 -0500

    added Card image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,18 @@
-import {Card,Button} from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap';
 
-export function PriceCard (props) {
-
-    return(
+export function PriceCard(props) {
+  return (
     <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-        <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>Price</Card.Text>
-    </Card.Body>
-    <Button> Add to Cart</Button>
+      <Card.Img
+        variant='top'
+        style={{ width: '80%', height: '30vh', margin: '2vh auto auto auto' }}
+        src='./hair.jpg'
+      />
+      <Card.Body>
+        <Card.Title>Rogaine</Card.Title>
+        <Card.Text>$1,000,000</Card.Text>
+      </Card.Body>
+      <Button> Add to Cart</Button>
     </Card>
-
-    )
+  );
 }
